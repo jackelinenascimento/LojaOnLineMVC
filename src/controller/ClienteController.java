8d024950c0f8cb85eafa4f3c6cdaf9f2aa777872
Jackeline do Nascimento <jackeline_nascimento@hotmail.com>
2020-09-21 12:45:29 -0300

    concluida estrutura cliente

--- src/controller/ClienteController.java
+++ src/controller/ClienteController.java
@@ -1,5 +1,65 @@
 package controller;
 
-public class ClienteController {
+import java.time.LocalDate;
 
+import model.Cliente;
+import view.ClienteView;
+
+public class ClienteController {
+	
+	private Cliente model;
+	private ClienteView view;
+	
+	public ClienteController(Cliente model, ClienteView view) {
+		this.model = model;
+		this.view = view;
+	}
+
+	public int getClienteId_cliente() {
+		return model.getId_cliente();
+	}
+	
+	public String getClienteNome() {
+		return model.getNome();
+	}
+	
+	public void setClienteNome(String nome) {
+		model.setNome(nome);
+	}
+	
+	public String getClienteCPF() {
+		return model.getCpf();
+	}
+	
+	public void setClienteCPF(String cpf) {
+		model.setCpf(cpf);
+	}
+	
+	public String getClienteRG() {
+		return model.getRg();
+	}
+	
+	public void setClienteRG(String rg) {
+		model.setRg(rg);
+	}
+	
+	public String getClienteEmail() {
+		return model.getEmail();
+	}
+	
+	public void setClienteEmail(String email) {
+		model.setEmail(email);
+	}
+	
+	public LocalDate getClienteDataNascimento() {
+		return model.getData_nascimento();
+	}
+	
+	public void setClienteDataNascimento(LocalDate data) {
+		model.setData_nascimento(data);
+	}
+	
+	public void updateView() {
+		view.printClienteDetails(model.getId_cliente(), model.getNome(), model.getCpf(),model.getRg(),model.getEmail(), model.getData_nascimento());
+	}
 }
--- src/model/Cliente.java
+++ src/model/Cliente.java
@@ -10,52 +10,60 @@ public class Cliente {
 	private String email;
 	private LocalDate data_nascimento;
 	
-	public Cliente() {}
-	
 	public Cliente(int id_cliente, String nome, String cpf, String rg, String email, LocalDate data_nascimento) {
-		super();
 		this.id_cliente = id_cliente;
 		this.nome = nome;
 		this.cpf = cpf;
 		this.rg = rg;
 		this.email = email;
-		this.data_nascimento = data_nascimento;
-	}
-	public int getId_cliente() {
-		return id_cliente;
-	}
-	public void setId_cliente(int id_cliente) {
-		this.id_cliente = id_cliente;
-	}
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	public String getRg() {
-		return rg;
-	}
-	public void setRg(String rg) {
-		this.rg = rg;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public LocalDate getData_nascimento() {
-		return data_nascimento;
-	}
-	public void setData_nascimento(LocalDate data_nascimento) {
 		this.data_nascimento = data_nascimento;
 	}
 
+	public int getId_cliente() {
+		return id_cliente;
+	}
+
+	public void setId_cliente(int id_cliente) {
+		this.id_cliente = id_cliente;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getRg() {
+		return rg;
+	}
+
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getData_nascimento() {
+		return data_nascimento;
+	}
+
+	public void setData_nascimento(LocalDate data_nascimento) {
+		this.data_nascimento = data_nascimento;
+	}
 }
